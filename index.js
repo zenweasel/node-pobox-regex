3b75ba79bb7b6b7e776946d5b1faccad257fdfe9
Brent Hoover <brent@thebuddhalodge.com>
2020-07-01 12:00:56 +0800

    (feat) Add linting

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-
 function matchAddressByRegex(destination, shippingAddress) {
-	if (destination.addressPattern) {
-    	const { addressPatternShouldMatch = true } = destination;
-    	const fullAddress = [shippingAddress.address1, shippingAddress.address2].join(" ");
-    	const addressMatchesPattern = RegExp(destination.addressPattern, "ig").test(fullAddress);
-    	if ((addressMatchesPattern && addressPatternShouldMatch) || (!addressMatchesPattern && !addressPatternShouldMatch)) {
-      		return true;
-    	}
-  	}
+  if (destination.addressPattern) {
+    const { addressPatternShouldMatch = true } = destination;
+    const fullAddress = [shippingAddress.address1, shippingAddress.address2].join(' ');
+    const addressMatchesPattern = RegExp(destination.addressPattern, 'ig').test(fullAddress);
+    if ((addressMatchesPattern && addressPatternShouldMatch)
+      || (!addressMatchesPattern && !addressPatternShouldMatch)) {
+      return true;
+    }
+  }
+  return false;
 }
 
 module.exports = matchAddressByRegex;
--- index.test.js
+++ index.test.js
@@ -1,46 +1,44 @@
-const matchAddressByRegex = require("./index.js");
+const matchAddressByRegex = require('./index.js');
 
 const destination = {
-	addressPatternShouldMatch: true,
-	addressPattern: "\\b[p]*(ost)*\\.*\\s*[o|0]*(ffice)*\\.*\\s*b[o|0]x\\b"
-}
+  addressPatternShouldMatch: true,
+  addressPattern: '\\b[p]*(ost)*\\.*\\s*[o|0]*(ffice)*\\.*\\s*b[o|0]x\\b',
+};
 
-describe("Should match a simple PO Box", () => {
+describe('Should match a simple PO Box', () => {
+  test('with periods', () => {
+    const shippingAddress = {
+      address1: 'P.O. Box 123',
+      address2: ' ',
+    };
+    const poBoxMatch1 = matchAddressByRegex(destination, shippingAddress);
+    expect(poBoxMatch1).toEqual(true);
+  });
 
-	test("with periods", () => {
-		const shippingAddress = {
-			address1: "P.O. Box 123",
-			address2:  " "
-		}
-		const poBoxMatch1 = matchAddressByRegex(destination, shippingAddress);
-		expect(poBoxMatch1).toEqual(true);	
-	})
+  test('without periods', () => {
+    const shippingAddress = {
+      address1: 'PO Box 123',
+      address2: ' ',
+    };
+    const poBoxMatch1 = matchAddressByRegex(destination, shippingAddress);
+    expect(poBoxMatch1).toEqual(true);
+  });
 
-	test("without periods", () => {
-		const shippingAddress = {
-			address1: "PO Box 123",
-			address2:  " "
-		}
-			const poBoxMatch1 = matchAddressByRegex(destination, shippingAddress);
-			expect(poBoxMatch1).toEqual(true);	
-	})
+  test('full words', () => {
+    const shippingAddress = {
+      address1: 'Post Office Box 123',
+      address2: '',
+    };
+    const poBoxMatch1 = matchAddressByRegex(destination, shippingAddress);
+    expect(poBoxMatch1).toEqual(true);
+  });
 
-
-	test("full words", () => {
-		const shippingAddress = {
-			address1: "Post Office Box 123",
-			address2:  ""
-		}
-			const poBoxMatch1 = matchAddressByRegex(destination, shippingAddress);
-			expect(poBoxMatch1).toEqual(true);	
-	})
-
-	test("split across two lines", () => {
-		const shippingAddress = {
-			address1: "Post Office",
-			address2:  "Box 123"
-		}
-			const poBoxMatch1 = matchAddressByRegex(destination, shippingAddress);
-			expect(poBoxMatch1).toEqual(true);	
-	})
-})
+  test('split across two lines', () => {
+    const shippingAddress = {
+      address1: 'Post Office',
+      address2: 'Box 123',
+    };
+    const poBoxMatch1 = matchAddressByRegex(destination, shippingAddress);
+    expect(poBoxMatch1).toEqual(true);
+  });
+});
