f84618a938c79624fc89ec07bddb96aecf83f029
Brent Hoover <brent@thebuddhalodge.com>
2020-07-01 11:30:47 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+function matchAddressByRegex(destination, shippingAddress) {
+	if (destination.addressPattern) {
+    	const { addressPatternShouldMatch = true } = destination;
+    	const fullAddress = [shippingAddress.address1, shippingAddress.address2].join(" ");
+    	const addressMatchesPattern = RegExp(destination.addressPattern, "ig").test(fullAddress);
+    	if ((addressMatchesPattern && addressPatternShouldMatch) || (!addressMatchesPattern && !addressPatternShouldMatch)) {
+      		return true;
+    	}
+  	}
+}
+
+module.exports = matchAddressByRegex;
